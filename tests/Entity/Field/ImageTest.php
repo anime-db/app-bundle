2c665d9ec7129c97b04ed1841e553ae8b878dae6
Peter Gribanov <info@peter-gribanov.ru>
2014-09-18 16:22:43 +0400

    test image field

--- tests/Entity/Field/ImageTest.php
+++ tests/Entity/Field/ImageTest.php
@@ -57,17 +57,6 @@ class ImageTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * Test set and get local file
-     */
-    public function testLocal()
-    {
-        $this->assertNull($this->image->getLocal());
-
-        $this->image->setLocal($this->file);
-        $this->assertEquals($this->file, $this->image->getLocal());
-    }
-
-    /**
      * Test has image
      */
     public function testHasImage()
@@ -84,7 +73,54 @@ class ImageTest extends \PHPUnit_Framework_TestCase
     public function testHasImageLocal()
     {
         $this->image->setLocal($this->file);
-
         $this->assertTrue($this->image->hasImage());
+    }
+
+    /**
+     * Get methods
+     *
+     * @return array
+     */
+    public function getMethods()
+    {
+        $this->setUp();
+        return [
+            ['getLocal', 'setLocal', null, $this->file],
+            ['getRemote', 'setRemote', '', 'http://example.com/foo'],
+            ['getFilename', 'setFilename', '', 'foo', 'tmp/'.date('Ymd').'/foo'],
+            ['getFilename', 'setFilename', '', 'tmp/20141018/bar']
+        ];
+    }
+
+    /**
+     * Test getters and setters
+     *
+     * @dataProvider getMethods
+     *
+     * @param callback $getter
+     * @param callback $setter
+     * @param mixed $default
+     * @param mixed $new
+     * @param mixed $expected
+     */
+    public function testGetSet($getter, $setter, $default, $new, $expected = null)
+    {
+        $expected = !is_null($expected) ? $expected : $new;
+        $this->assertEquals($default, call_user_func([$this->image, $getter]));
+
+        call_user_func([$this->image, $setter], $new);
+        $this->assertEquals($expected, call_user_func([$this->image, $getter]));
+    }
+
+    /**
+     * Test clear
+     */
+    public function testClear()
+    {
+        $this->image->setLocal($this->file);
+        $this->image->setRemote('http://example.com/foo');
+        $this->image->clear();
+        $this->assertEmpty($this->image->getLocal());
+        $this->assertEmpty($this->image->getRemote());
     }
 }
